ac74d883dca1d4ce40437a29f7a9bc700aad0579
suraj kengar <surajkengar1212@gmail.com>
2025-10-29 11:41:56 +0530

    server created

---
+++ src/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const app = express();
+app.use("/test",(req,res)=>{
+    res.send("test is done successfully");
+})
+app.use("/index",(req,res)=>{
+    res.send("go to index page");
+})
+app.use("/home" , (req,res)=>{
+    res.send("go to home page");
+})
+app.use("/read",(req,res)=>{
+    res.send("read the all docunment succcessfully");
+})
+app.use((req,res)=>{
+    res.send("server is started successfully");
+})
+app.listen(3000,()=>{
+    console.log("server starred successfully");
+} );
